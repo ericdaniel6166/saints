24013ee4984f947ad4a302c8137c3e7020473765
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-27 17:41:54 +0000

    Deploy eudoxos/saints to github.com/eudoxos/saints.git:gh-pages

--- epub/_static/documentation_options.js
+++ epub/_static/documentation_options.js
@@ -1,9 +1,11 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '51f4c79',
+    VERSION: 'd76a4de',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'epub',
     FILE_SUFFIX: '.xhtml',
+    LINK_SUFFIX: '.xhtml',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
     NAVIGATION_WITH_KEYS: false
